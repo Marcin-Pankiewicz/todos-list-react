a5587986e42d1a32eab70b1e1b64e70ae396f3f7
Marcin Pankiewicz <pankiewicz.marcin@gmail.com>
2021-06-15 22:48:07 +0200

    added media quaries and flex for option buttons

--- src/Section/index.js
+++ src/Section/index.js
@@ -1,10 +1,10 @@
 import "./style.css";
 
-const Section = ({ title, body, extraHeaderSpan }) => (
+const Section = ({title, body, extraHeaderSpan}) => (
     <section className="section">
         <h2 className="section__header">{title} {extraHeaderSpan}</h2>
         <div className="section__body">
-            {body}
+            {body} 
         </div>
     </section>
 );
